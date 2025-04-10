07a83c094f751e22e82e4c32933a578ff71f3612
Elizaveta <62665596+sharkgrin1@users.noreply.github.com>
2025-04-10 16:19:01 +1200

    dynamic job descriptions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
+import 'material-icons/iconfont/material-icons.css';
+import {createApp} from 'vue'
 import App from "@/App.vue";
 
 createApp(App).mount('#app')
