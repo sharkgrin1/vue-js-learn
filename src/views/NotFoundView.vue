54140d9045f4e4a715a0281479f8ab577186e692
Elizaveta <62665596+sharkgrin1@users.noreply.github.com>
2025-04-15 20:05:50 +1200

    not found page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="text-center flex flex-col justify-center items-center h-120">
+    <span class="material-icons md-80 text-yellow-600">warning</span>
+    <h1 class="text-6xl mb-4">404 Not Found</h1>
+    <p class="text-xl mb-5">Page does not exist</p>
+    <RouterLink to="/" class="text-white bg-gray-600 hover:bg-gray-800 rounded-md px-3 py-2">
+      Return to Home
+    </RouterLink>
+  </section>
+</template>
+
+<style scoped>
+.material-icons.md-80 {
+  font-size: 80px;
+}
+</style>
